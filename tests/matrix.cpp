d89ea7bec400bafa09f1d02d5985ab74dbab3590
KadrusBAG <32191410+KadrusBAG@users.noreply.github.com>
2018-03-22 11:44:47 +0300

    Update matrix.cpp

--- tests/matrix.cpp
+++ tests/matrix.cpp
@@ -30,6 +30,26 @@ TEST_CASE("reading matrix")
     REQUIRE( input == ostream.str() );
 }
 
+TEST_CASE("reading float_matrix")
+{
+    std::string input{
+        "3, 3\n"
+        "1.2 1.3 1.4\n"
+        "2.3 2.4 2.5\n"
+        "3.4 3.5 3.6" };
+    matrix_t<float> matrix;
+    std::istringstream stream{ input };
+    
+    REQUIRE( matrix.read( stream ) );
+    REQUIRE( matrix.rows() == 3 );
+    REQUIRE( matrix.collumns() == 3 );
+    
+    std::ostringstream output;
+    matrix.write( output );
+    
+    REQUIRE( input == output.str() );
+}
+
 TEST_CASE("add matrix")
 {
     std::string stroka1{
@@ -151,63 +171,63 @@ TEST_CASE("selfsub matrix")
     REQUIRE( strokar == ostream.str() );
 }
 
-TEST_CASE("selfadd matrix")
+TEST_CASE("Iskluchenie 1")
 {
-    std::string stroka1{
-        "2, 3\n"
-        "1.1 1.1 1.1\n"
-        "2.2 2.2 2.2" }; 
-    std::string stroka2{
-        "2, 3\n"
-        "2.2 2.2 2.2\n"
-        "1.1 1.1 1.1" };
-    std::string strokar{
-        "2, 3\n"
-        "3.3 3.3 3.3\n"
-        "3.3 3.3 3.3" };
+    std::string first_mat{
+        "3, 3\n"
+        "1 2 3\n"
+        "1 2 3\n"
+        "1 2 3\n"
+    };
+    std::string second_mat{
+        "3, 3\n"
+        "3 2 1\n"
+        "3 2 1\n"
+        "3 2 1\n"
+    };
     
-    matrix_t<float> matrix1, matrix2;   
-    std::istringstream istream1{ stroka1 };
-    std::istringstream istream2{ stroka2 };
+    std::istringstream stream1{ first_mat };
+    matrix_t<int> first_matrix;
+    first_matrix.read( stream1 );
     
-    matrix1.read( istream1 );
-    matrix2.read( istream2 );
+    std::istringstream stream2 { second_mat };
+    matrix_t<int> second_matrix;
+    second_matrix.read( stream2 );
     
-    matrix1 += matrix2;
+    char op = '+';
+    bool proverka_ = first_matrix.success (second_matrix, op);
+    bool expect = true;
     
-    std::ostringstream ostream;
-    matrix1.write( ostream );
-    
-    REQUIRE( strokar == ostream.str() );
+    REQUIRE( proverka_ == expect );
 }
 
-TEST_CASE("selfmul matrix")
+TEST_CASE("Iskluchenie 2")
 {
-    std::string stroka1{
-        "2, 3\n"
+    std::string first_mat{
+        "3, 3\n"
         "1 2 3\n"
-        "4 5 6" }; 
-    std::string stroka2{
+        "1 2 3\n"
+        "1 2 3\n"
+    };
+    std::string second_mat{
         "3, 2\n"
-        "1 4\n"
-        "2 5\n"
-        "3 6"  };
-    std::string strokar{
-        "2, 2\n"
-        "14 32\n"
-        "32 77" };
+        "3 2\n"
+        "3 2\n"
+        "3 2\n"
+    };
     
-    matrix_t<int> matrix1, matrix2;   
-    std::istringstream istream1{ stroka1 };
-    std::istringstream istream2{ stroka2 };
+    std::istringstream stream1{ first_mat };
+    matrix_t<int> first_matrix;
+    first_matrix.read( stream1 );
     
-    matrix1.read( istream1 );
-    matrix2.read( istream2 );
+    std::istringstream stream2 { second_mat };
+    matrix_t<int> second_matrix;
+    second_matrix.read( stream2 );
     
-    matrix1 *= matrix2;
+    char op = '+';
+    bool proverka_ = first_matrix.success (second_matrix, op);
+    bool expect = false;
     
-    std::ostringstream ostream;
-    matrix1.write( ostream );
-    
-    REQUIRE( strokar == ostream.str() );
+    REQUIRE( proverka_ == expect );
 }
+
